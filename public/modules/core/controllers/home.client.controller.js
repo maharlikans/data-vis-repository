72f368345b3c9c6a7d2ad22d526b4a0280825f87
Kyle Maharlika <kyle.maharlika@gmail.com>
2015-04-10 16:44:16 -0400

    Added single list of items to display to core controller

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,22 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', '$stateParams', '$location','Authentication', 'Links', 'Papers', 'Videos', '$modal', '$log',
+	function($scope, $stateParams, $location, Authentication, Links, Papers, Videos, $modal, $log) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+    $scope.items = [];
+
+		$scope.find = function(items) {
+      var populate = function(elements) {
+        for(var i = 0; i < elements.length; i++) {
+          items.push(elements[i]);
+        }
+      };
+
+      var links = Links.query(populate);
+      var papers = Papers.query(populate);
+      var videos = Videos.query(populate);
+		};
 	}
 ]);
