ef126fbcf801a9f974a836b2a0d12ac9a073aba4
Kyle Maharlika <kyle.maharlika@gmail.com>
2015-04-11 18:44:24 -0400

    Added masonry functionality and ordering

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'data-visualization-repository';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils'];
+	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils', 'wu.masonry'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName, dependencies) {
